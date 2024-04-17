d93e460d2d0dc75c4a5cfbb44d8d2f1c7488a3e0
Basanta-Karki <dk1684435@gmail.com>
2024-04-17 21:12:13 +0545

    Landing page and fruits page of the farmkit that uses tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import LandingPage from "./components/LandingPage";
+import Fruits from "./components/Fruits";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing necessary components from react-router-dom
+
+// Main App component
+const App = () => {
+  return (
+    <Router>
+      {" "}
+      {/* Router component to provide navigation functionality */}
+      <div>
+        {" "}
+        {/* Main container for all routes */}
+        <Routes>
+          {" "}
+          {/* Routes component for defining different routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/fruits" element={<Fruits />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App; // Exporting the App component
