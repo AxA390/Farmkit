f567dff92045b1773c7b569668622711bd604971
Basanta-Karki <dk1684435@gmail.com>
2024-04-18 15:59:54 +0545

    Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 import React from "react";
-import LandingPage from "./components/LandingPage";
-import Fruits from "./components/Fruits";
-import Apples from "./components/Apples";
-import Profile from "./components/Profile";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing necessary components from react-router-dom
+// import LandingPage from "./components/LandingPage";
+// import Fruits from "./components/Fruits";
+// import Apples from "./components/Apples";
+// import Profile from "./components/Profile";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing necessary components from react-router-dom
+// import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 // Main App component
 const App = () => {
   return (
-    <Router>
-      {" "}
-      {/* Router component to provide navigation functionality */}
-      <div>
-        {" "}
-        {/* Main container for all routes */}
-        <Routes>
-          {" "}
-          {/* Routes component for defining different routes */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/fruits" element={<Fruits />} />
-          <Route path="/apples" element={<Apples />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   {" "}
+    //   {/* Router component to provide navigation functionality */}
+    //   <div>
+    //     {" "}
+    //     {/* Main container for all routes */}
+    //     <Routes>
+    //       {" "}
+    //       {/* Routes component for defining different routes */}
+    //       <Route path="/" element={<LandingPage />} />
+    //       <Route path="/fruits" element={<Fruits />} />
+    //       <Route path="/apples" element={<Apples />} />
+    //       <Route path="/profile" element={<Profile />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+
+    <Signup />
   );
 };
 
